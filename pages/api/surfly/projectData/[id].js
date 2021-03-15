13e8cf26880e3a5601e8689bcc9f337f599127fe
Stefanibus <stefan.brueck@posteo.de>
2021-03-15 22:05:40 +0100

    fixed Styling-Import-Issue in Surfly API ProjectData-Route

--- pages/api/surfly/projectData/[id].js
+++ pages/api/surfly/projectData/[id].js
@@ -12,8 +12,8 @@ export default async (req, res) => {
           res.status(404).json({ success: false });
         }
         const jsResult    = ` ${data.js} ` ;
-        const cssResult   = ` ; const style = document.createElement('style'); style.innerHTML =   ' ${data.css}    ' ; document.head.appendChild(style); ` ;
-        const cssScrollBar   = ` ; const style = document.createElement('style'); style.innerHTML =   ' #::-webkit-scrollbar{width:9px}::-webkit-scrollbar-track{box-shadow:inset 0 0 5px grey;border-radius:30px;margin-top:5px;margin-bottom:5px}::-webkit-scrollbar-thumb{background:#a9a9a9;border-radius:60px}::-webkit-scrollbar-thumb[_ss_hover]{background:#a9a9a9}    ' ; document.head.appendChild(style); ` ;
+        const cssResult   = ` ; const User_Input_Style = document.createElement('style'); style.innerHTML =   ' ${data.css}    ' ; document.head.appendChild(User_Input_Style); ` ;
+        const cssScrollBar   = ` ; const ScrollBarStyle = document.createElement('style'); style.innerHTML =   ' #::-webkit-scrollbar{width:9px}::-webkit-scrollbar-track{box-shadow:inset 0 0 5px grey;border-radius:30px;margin-top:5px;margin-bottom:5px}::-webkit-scrollbar-thumb{background:#a9a9a9;border-radius:60px}::-webkit-scrollbar-thumb[_ss_hover]{background:#a9a9a9}    ' ; document.head.appendChild(ScrollBarStyle); ` ;
         const fullResult  = jsResult + cssResult + cssScrollBar   ;
         const surflyInjection  =   fullResult     ;
          res.
