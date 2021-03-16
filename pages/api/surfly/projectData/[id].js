00d21ac8d59f5c6a53a5fcc41e87df450166cce7
Stefanibus <stefan.brueck@posteo.de>
2021-03-16 11:32:54 +0100

    Cors preflights prevented testing on localhost, Finally fixed Styling

--- pages/api/surfly/projectData/[id].js
+++ pages/api/surfly/projectData/[id].js
@@ -12,10 +12,10 @@ export default async (req, res) => {
           res.status(404).json({ success: false });
         }
         const jsResult    = ` ${data.js} ` ;
-        const cssUserResult   = "  var cssUserResult =  `" + data.css +  " ` ; var head = document.head || document.getElementsByTagName('head')[0]; var scrollBarStyle = document.createElement('style');  head.appendChild(scrollBarStyle);  scrollBarStyle.type = 'text/css';  scrollBarStyle.appendChild(document.createTextNode(cssUserResult)); ";
-        const cssScrollBar   = "  var cssScrollbar =  `   #::-webkit-scrollbar{width:9px}::-webkit-scrollbar-track{box-shadow:inset 0 0 5px grey;border-radius:30px;margin-top:5px;margin-bottom:5px}::-webkit-scrollbar-thumb{background:#a9a9a9;border-radius:60px}::-webkit-scrollbar-thumb[_ss_hover]{background:#a9a9a9}  ` ; var head = document.head || document.getElementsByTagName('head')[0]; var scrollBarStyle = document.createElement('style');  head.appendChild(scrollBarStyle);  scrollBarStyle.type = 'text/css';  scrollBarStyle.appendChild(document.createTextNode(cssScrollbar)); ";
+        const cssUserResult   = "  var cssUserResult =    ` "+ data.css  +"  `     ; var head = document.head || document.getElementsByTagName('head')[0]; var scrollBarStyle = document.createElement('style');  head.appendChild(scrollBarStyle);  scrollBarStyle.type = 'text/css';  scrollBarStyle.appendChild(document.createTextNode(cssUserResult)); ";
+        const cssScrollBar   = "  var cssScrollbar =    `  ::-webkit-scrollbar{width:9px}::-webkit-scrollbar-track{box-shadow:inset 0 0 5px grey;border-radius:30px;margin-top:5px;margin-bottom:5px}::-webkit-scrollbar-thumb{background:#a9a9a9;border-radius:60px}::-webkit-scrollbar-thumb[_ss_hover]{background:#a9a9a9}    `   ; var head = document.head || document.getElementsByTagName('head')[0]; var scrollBarStyle = document.createElement('style');  head.appendChild(scrollBarStyle);  scrollBarStyle.type = 'text/css';  scrollBarStyle.appendChild(document.createTextNode(cssScrollbar)); ";
 
-        const fullResult  = jsResult + cssResult + cssScrollBar   ;
+        const fullResult  = jsResult + cssUserResult + cssScrollBar   ;
         const surflyInjection  =   fullResult     ;
          res.
          setHeader('Content-Type', 'application/javascript') ;
