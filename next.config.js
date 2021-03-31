ff4949bddd3e0a8f76980f742087c35c0a1fbc82
Stefanibus <stefan.brueck@posteo.de>
2021-03-31 12:06:27 +0200

    tested webpack5 but then reverted back to webpack4
    thus I still have the   loding-isse on (never stops loading) http://localhost:3000/_next/webpack-hmr?page=/

--- next.config.js
+++ next.config.js
@@ -2,8 +2,10 @@
 // but did not solve the loding-isse on (never stops loading) http://localhost:3000/_next/webpack-hmr?page=/
 // only added a new issue with caching fsevents in chokidar
 // https://nextjs.org/docs/messages/webpack5
-//  module.exports = {
-//   future: {
-//     webpack5: true,
-//   },
-// }
+// the only workaround I found was to block that network request clientside : https://content.screencast.com/users/stefanibus/folders/Capture/media/07c27686-0a34-4f1a-9679-6929002dc0f8/LWR_Recording.png
+
+ module.exports = {
+  future: {
+    webpack5: false,
+  },
+}
