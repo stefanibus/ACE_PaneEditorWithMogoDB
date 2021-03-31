4a728df30458f658b29640816aa2fa97c8cbd23f
Stefanibus <stefan.brueck@posteo.de>
2021-03-31 11:53:19 +0200

    tested webpack5 but then reverted back to webpack4
    thus I still have the   loding-isse on (never stops loading) http://localhost:3000/_next/webpack-hmr?page=/

---
+++ next.config.js
@@ -0,0 +1,9 @@
+// The below code only added new issues
+// but did not solve the loding-isse on (never stops loading) http://localhost:3000/_next/webpack-hmr?page=/
+// only added a new issue with caching fsevents in chokidar
+// https://nextjs.org/docs/messages/webpack5
+//  module.exports = {
+//   future: {
+//     webpack5: true,
+//   },
+// }
