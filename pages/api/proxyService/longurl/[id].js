493b989f4d52ff07aea891caea7239ca2e3be13c
Stefanibus <stefan.brueck@posteo.de>
2021-05-05 18:14:17 +0200

    test heroku issue

--- pages/api/proxyService/longurl/[id].js
+++ pages/api/proxyService/longurl/[id].js
@@ -5,27 +5,28 @@ export default async (req, res) => {
   const { id } = req.query
 
 
-  console.log('okay logged to console')
-  return  res.send(  'collectionName2 = ' + collectionName2 + ' id = ' + id + '.   okay That is all folks'  );
+  // console.log('okay logged to console')
+  // return  res.send(  'collectionName2 = ' + collectionName2 + ' id = ' + id + '.   okay That is all folks.'  );
 
-      // try {
-      //   const { db } = await connect();
-      //   const data = await db
-      //     .collection(collectionName2)
-      //     .findOne({ _id: ObjectId(id) });
-      //   if (!data) {
-      //     res.status(404).json({ success: false });
-      //     // stop further execution in this callback
-      //     return;
-      //   }
-      //    res.setHeader('Content-Type', 'application/javascript') ;
-      //   let longUrlResponse = data.longurl
-      //   res.send(  {longUrlResponse}  );
-      //   // console.log('SURFLY GET LongURL-STRING successfully');
-      // } catch (error) {
-      //   res.
-      //   status(500).json({ success: false });
-      //   // stop further execution in this callback
-      //   return;
-      // }
+      try {
+        const { db } = await connect();
+        const data = await db
+          .collection(collectionName2)
+          .findOne({ _id: ObjectId(id) });
+        if (!data) {
+          res.status(404).json({ success: false });
+          // stop further execution in this callback
+          return;
+        }
+         res.setHeader('Content-Type', 'application/javascript') ;
+        let longUrlResponse = data.longurl
+        // console.log('SURFLY GET LongURL-STRING successfully');
+       return  res.send(  {longUrlResponse}  );
+
+      } catch (error) {
+        res.
+        status(500).json({ success: false });
+        // stop further execution in this callback
+        return;
+      }
 };
