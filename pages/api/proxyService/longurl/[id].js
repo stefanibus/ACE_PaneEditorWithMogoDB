cfb89641176c4357466b9221bbe7f5be202536fe
Stefanibus <stefan.brueck@posteo.de>
2021-05-05 16:49:57 +0200

    test heroku issue

--- pages/api/proxyService/longurl/[id].js
+++ pages/api/proxyService/longurl/[id].js
@@ -1,26 +1,31 @@
-import { connect, ObjectId } from "../../../../utils/database";
+//  import { connect, ObjectId } from "../../../../utils/database";
 
 export default async (req, res) => {
   const collectionName2 = "pensName";
   const { id } = req.query
-      try {
-        const { db } = await connect();
-        const data = await db
-          .collection(collectionName2)
-          .findOne({ _id: ObjectId(id) });
-        if (!data) {
-          res.status(404).json({ success: false });
-          // stop further execution in this callback
-          return;
-        }
-         res.setHeader('Content-Type', 'application/javascript') ;
-        let longUrlResponse = data.longurl
-        res.send(  {longUrlResponse}  );
-        // console.log('SURFLY GET LongURL-STRING successfully');
-      } catch (error) {
-        res.
-        status(500).json({ success: false });
-        // stop further execution in this callback
-        return;
-      }
+
+
+  console.log('okay logged to console')
+  return  res.send(  'collectionName2 = ' + collectionName2 + ' id = ' + id + '.   okay That is all folks'  );
+
+      // try {
+      //   const { db } = await connect();
+      //   const data = await db
+      //     .collection(collectionName2)
+      //     .findOne({ _id: ObjectId(id) });
+      //   if (!data) {
+      //     res.status(404).json({ success: false });
+      //     // stop further execution in this callback
+      //     return;
+      //   }
+      //    res.setHeader('Content-Type', 'application/javascript') ;
+      //   let longUrlResponse = data.longurl
+      //   res.send(  {longUrlResponse}  );
+      //   // console.log('SURFLY GET LongURL-STRING successfully');
+      // } catch (error) {
+      //   res.
+      //   status(500).json({ success: false });
+      //   // stop further execution in this callback
+      //   return;
+      // }
 };
--- utils/database.js
+++ utils/database.js
@@ -17,3 +17,38 @@ async function connect() {
 }
 
 export { connect, ObjectId };
+
+
+
+
+
+// ##############################
+
+
+// import { MongoClient } from 'mongodb'
+
+// let uri = process.env.MONGO_URI
+// let dbName = process.env.MONGO_DB
+
+// let promise = null
+// let cached = null
+
+// if (!uri) throw new Error('Missing environment variable MONGO_URI')
+// if (!dbName) throw new Error('Missing environment variable MONGO_DB')
+
+// export async function connectToDatabase() {
+//   if (cached) return cached
+//   if (!promise) {
+//     promise = MongoClient.connect(uri, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     })
+//   }
+//   const client = await promise
+//   const db = await client.db(dbName)
+//   cached = {
+//     client,
+//     db,
+//   }
+//   return cached
+// }
